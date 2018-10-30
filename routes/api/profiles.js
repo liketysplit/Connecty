bf6671a3b133ddd641837f63fa9539902d18de42
liketysplit <rjboles2@gmail.com>
2018-10-29 23:55:39 -0400

    2.3 Routes added for profiles, posts, and users

---
+++ routes/api/profiles.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   GET /api/profiles/test
+// @desc    Tests profiles route
+// @access  Public
+router.get("/test", (req, res) => res.json({
+    msg: "Profiles works."
+})); // The route implies /api/profiles/test
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
 const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const profiles = require("./routes/api/profiles");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
@@ -18,6 +20,12 @@ mongoose
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("hello!"));
+
+// Use routes
+app.use("/api/users", users);
+app.use("/api/profiles", profiles);
+app.use("/api/posts", posts);
+
 
 // The actual port for the app in prodcution will come from the env. var. "PORT". For local dev., we just use 5000.
 const port = process.env.PORT || 5000;
