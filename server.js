26e6dedbbf5642ddce7642bb5a872d2956800ee1
liketysplit <rjboles2@gmail.com>
2018-11-20 06:51:55 -0500

    Reloaded Modules from Heroku Documentation

--- server.js
+++ server.js
@@ -21,8 +21,12 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-    .connect(db)
-    .then(() => console.log('MongoDB Connected'))
+    .connect(
+        db, {
+            useNewUrlParser: true
+        }
+    )
+    .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
 // Passport middleware
