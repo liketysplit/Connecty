99740bc684627f353c6a4c18fa858635fc4bd0cc
liketysplit <rjboles2@gmail.com>
2018-11-20 06:09:58 -0500

    Testing Again

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const path = require('path');
 
 const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
+const profile = require('./routes/api/profiles');
 const posts = require('./routes/api/posts');
 
 const app = express();
@@ -33,7 +33,7 @@ require('./config/passport')(passport);
 
 // Use Routes
 app.use('/api/users', users);
-app.use('/api/profile', profile);
+app.use('/api/profiles', profile);
 app.use('/api/posts', posts);
 
 // Server static assets if in production
