9255326e00ac155828400efca0b6c50d10b52602
liketysplit <rjboles2@gmail.com>
2018-12-02 16:33:20 -0500

    6.2.1 Action Types && 6.2.2 Reducers

--- client/src/App.js
+++ client/src/App.js
@@ -14,10 +14,13 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+import rootReducer from "./reducers";
+
+const initialState = {};
 const middleware = [thunk];
 const store = createStore(
-	() => [], // empty store since no reducer created yet
-	{},
+	rootReducer,
+	initialState,
 	composeWithDevTools(applyMiddleware(...middleware))
 );
 
